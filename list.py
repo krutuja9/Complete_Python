14d7eebec9acb3c28ef5696c96c44dfc38f62305
krutuja9 <krutuja609@gmail.com>
2024-10-18 16:43:20 +0530

    List

---
+++ list.py
@@ -0,0 +1,25 @@
+#  list = used to store multiple items in a single variable
+
+food = ["Pizza","Hamburger", "Hotdog","Magi","VadaPav"]
+
+food[0] = "Sushi"
+
+# print(food[0])
+# food.append("ice cream")
+# food.remove("VadaPav")
+# food.pop()
+# food.insert(0,"cake")
+# food.sort()
+# food.clear()
+# for x in food:
+#   print(x)
+
+
+#  2nd List = a list of lists
+drinks = ["coffee","soda","tea","lemonjuice",]
+dinner = [" pizza",'panir','hotdog']
+dessert = ['cake','icecream']
+
+food = [drinks,dinner,dessert]
+
+print(food[2][1])
