9afd663720509dc2e124648029dcdf2fe918c8ff
krutuja9 <krutuja609@gmail.com>
2024-10-18 17:00:06 +0530

    set

---
+++ set.py
@@ -0,0 +1,16 @@
+#  set = collection which is unordered , unindexed , no duplicate values
+
+utensil = {"fork","spoon","knife"}
+dishes = {"bowl","plate","cup","dish","knife"}
+
+# utensil.add("napkin")
+# utensil.remove("spoon")
+# utensil.clear()
+
+# utensil.update(dishes)
+# dinner_table = utensil.union(dishes)
+# for x in utensil:
+#   print(x)
+
+# print(utensil.difference(dishes))
+print(utensil.intersection(dishes))
